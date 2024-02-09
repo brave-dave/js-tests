5f9ac694c0a682e706d610ca1683ddf604e1d5a6
Olga <olginarom@gmail.com>
2024-02-09 13:26:13 +0100

    correzioni task 10, 11

--- src/test-10/index.test.ts
+++ src/test-10/index.test.ts
@@ -2,19 +2,19 @@ import { expect, test } from "vitest";
 import { sortNumbers } from ".";
 
 test("sortNumbers returns the same list ordered ascendant by default", () => {
-  expect(sortNumbers([10, 9, 8, 7, 6, 5, 4, 3, 2, 1])).toBe([
+  expect(sortNumbers([10, 9, 8, 7, 6, 5, 4, 3, 2, 1])).toStrictEqual([
     1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
   ]);
 });
 
 test("sortNumbers returns the same list ordered ascendant", () => {
-  expect(sortNumbers([10, 9, 8, 7, 6, 5, 4, 3, 2, 1], "asc")).toBe([
+  expect(sortNumbers([10, 9, 8, 7, 6, 5, 4, 3, 2, 1], "asc")).toStrictEqual([
     1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
   ]);
 });
 
 test("sortNumbers returns the same list ordered descendant", () => {
-  expect(sortNumbers([9, 8, 10, 7, 6, 5, 3, 2, 1, 4], "desc")).toBe([
+  expect(sortNumbers([9, 8, 10, 7, 6, 5, 3, 2, 1, 4], "desc")).toStrictEqual([
     10, 9, 8, 7, 6, 5, 4, 3, 2, 1,
   ]);
 });
--- src/test-10/index.ts
+++ src/test-10/index.ts
@@ -1,1 +1,9 @@
-export const sortNumbers = () => {};
+export const sortNumbers = (list: number[], order: string = "asc"): number[] => {
+    if (order === "desc") {
+        return list.sort((a, b) => b - a);
+    } else if (order === "asc") {
+        return list.sort((a, b) => a - b);
+    } else {
+        throw new Error("Invalid order parameter. Use 'asc' or 'desc'.");
+    }
+};
